bffad9291d6c95553b19ad3912141ff0cd5faa8c
Mohamed Azuhar <Z8Zuzane@hotmail.com>
2020-04-22 17:41:20 +0500

    initial

---
+++ main.dart
@@ -0,0 +1,124 @@
+import 'dart:io';
+
+void main(){
+  printIntro();
+
+  while(true){
+    var input = getOperation();
+    input = input.toLowerCase();
+
+  switch (input) {
+    case "+":
+    {
+      input = getInputOne();
+      int parsedOne = int.tryParse(input);
+
+      input = getInputTwo();
+      int parsedTwo = int.tryParse(input);
+
+      if(parsedTwo == null){
+          print("Couldnt parse to an integer");
+      }
+
+      print("$parsedOne + $parsedTwo = ${add(parsedOne, parsedTwo)}");
+    }
+    break;
+    case "-":
+    {
+       input = getInputOne();
+      int parsedOne = int.tryParse(input);
+
+      input = getInputTwo();
+      int parsedTwo = int.tryParse(input);
+
+      if(parsedTwo == null){
+          print("Couldnt parse to an integer");
+      }
+
+      print("$parsedOne - $parsedTwo = ${subtract(parsedOne, parsedTwo)}");
+    }
+    break;
+    case "*":
+    {
+       input = getInputOne();
+      int parsedOne = int.tryParse(input);
+
+      input = getInputTwo();
+      int parsedTwo = int.tryParse(input);
+
+      if(parsedTwo == null){
+          print("Couldnt parse to an integer");
+      }
+
+      print("$parsedOne x $parsedTwo = ${multiply(parsedOne, parsedTwo)}");
+    }
+    break;
+    case "/":
+    {
+      input = getInputOne();
+       
+      var parsedOne = double.tryParse(input);
+
+      input = getInputTwo();
+      var parsedTwo = double.tryParse(input);
+
+      if(parsedTwo == null){
+          print("Couldnt parse to a double");
+      }
+
+      print("$parsedOne / $parsedTwo = ${divide(parsedOne, parsedTwo)}");
+    }
+    break;
+    case "q":
+    {
+      exit(69);
+    }
+    break;
+    default:
+    {
+      print("Invalid Operation.");
+    }
+  }
+  }
+}
+
+void printIntro(){
+  print("#####################");
+  print("#    Calculator     #");
+  print("#       Add         #");
+  print("#     Subtract      #");
+  print("#      Divide       #");
+  print("#     Multiply      #");
+  print("#####################");
+}
+
+String getOperation(){
+  stdout.write("Enter operation: ");
+  return stdin.readLineSync();
+}
+
+String getInputOne(){
+  stdout.write("Enter number one: ");
+  return stdin.readLineSync();
+}
+
+String getInputTwo(){
+  stdout.write("Enter number two: ");
+  return stdin.readLineSync();
+}
+
+int add(int x, int y){
+  return x+y;
+}
+
+int subtract(int x, int y){
+  return x-y;
+}
+
+double divide(double x, double y){
+  return x/y;
+}
+
+int multiply(int x, int y){
+  return x*y;
+}
